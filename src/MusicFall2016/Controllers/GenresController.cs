85390c481dd67357f805f3f5d7b3e216e25ea4f3
daya1 <daya1@D017LAB-19>
2016-10-28 13:53:59 -0400

    last commit

--- src/MusicFall2016/Controllers/GenresController.cs
+++ src/MusicFall2016/Controllers/GenresController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MusicFall2016.Models;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -82,6 +83,28 @@ namespace MusicFall2016.Controllers
             return RedirectToAction("Details");
         }
 
+        public IActionResult Genre(int? id)
+        {
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+                var album = _context.Albums
+                .Include(a => a.Artist)
+                .Include(a => a.Genre)
+                .ToList();
 
+                var genre = _context.Genres.SingleOrDefault(a => a.GenreID == id);
+                ViewData["Genre"] = genre.Name;     //sends data to the Genre view
+
+                if (album == null)
+                {
+                    return NotFound();
+                }
+                return View(album);
+            }
+
+        }
     }
 }
