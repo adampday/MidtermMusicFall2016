340830fd46a88cf16f192d1396bdcd170e103507
aday <aday@DESKTOP-ATHQ5T4>
2016-11-02 17:22:39 -0400

    new commit

--- src/MusicFall2016/Models/Album.cs
+++ src/MusicFall2016/Models/Album.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-
+//new change
 namespace MusicFall2016.Models
 {
     public class Album
