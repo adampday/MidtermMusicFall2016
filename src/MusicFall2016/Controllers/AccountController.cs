39d1b27dac5f702b045b214b7287fc2ba08d0622
daya1 <daya1@D017LAB-19>
2016-11-06 15:11:48 -0500

    new commit

--- src/MusicFall2016/Controllers/AccountController.cs
+++ src/MusicFall2016/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using MusicFall2016.Models;
 using Microsoft.AspNetCore.Authorization;
 using MusicFall2016.ViewModels;
+using Microsoft.AspNetCore.Http.Authentication;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,6 +13,9 @@ public class AccountController : Controller
 {
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public Task DefaultAuthenticationTypes { get; private set; }
+
     public AccountController(
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager)
@@ -27,9 +31,32 @@ public class AccountController : Controller
     {
         if (ModelState.IsValid)
         {
-            var user = new ApplicationUser {UserName = model.Email};
+            var user = new ApplicationUser { UserName = model.Email };
             var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                await SignInAsync(user, isPersistent: false);
+            }
         }
         return View(model);
+    }
+
+    private async Task SignInAsync(ApplicationUser user, bool isPersistent)
+    {
+        var identity = await UserManager.CreateIdentityAsync(
+       user, DefaultAuthenticationTypes.ApplicationCookie);
+
+        AuthenticationManager.SignIn(
+           new AuthenticationProperties()
+           {
+               IsPersistent = isPersistent
+           }, identity);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult LogOff()
+    {
+        AuthenticationManager.SignOut();
     }
 }
---
+++ src/MusicFall2016/UserManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using MusicFall2016.Models;
+
+internal class UserManager
+{
+    internal static Task CreateIdentityAsync(ApplicationUser user, object applicationCookie)
+    {
+        throw new NotImplementedException();
+    }
+}
