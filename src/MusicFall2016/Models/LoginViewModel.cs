f82b1cf2430e01c9e46a70b9dce433db7de2d268
daya1 <daya1@D017LAB-19>
2016-11-09 15:12:10 -0500

    new commit

--- src/MusicFall2016/Models/LoginViewModel.cs
+++ src/MusicFall2016/Models/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace MusicFall2016.Models
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Email is required.")]
+        [RegularExpression("^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$", ErrorMessage = "Please enter valid email.")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
--- src/MusicFall2016/Models/RegisterViewModel.cs
+++ src/MusicFall2016/Models/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,15 @@ namespace MusicFall2016.Models
 {
     public class RegisterViewModel
     {
+        [Required(ErrorMessage = "Email is required.")]
+        [RegularExpression("^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$", ErrorMessage = "Please enter valid email.")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+        
+        [Compare("Password",ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
         public string confirmPassword { get; set; }
     }
 }
