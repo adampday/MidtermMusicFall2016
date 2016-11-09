502ec7b7a1147738b86993a4cb42214f310d3a5b
daya1 <daya1@D017LAB-19>
2016-11-09 13:08:34 -0500

    new commit

--- src/MusicFall2016/Controllers/AccountController.cs
+++ src/MusicFall2016/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using MusicFall2016.Models;
 using Microsoft.AspNetCore.Authorization;
 using MusicFall2016.ViewModels;
 using Microsoft.AspNetCore.Http.Authentication;
+using System;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -44,7 +45,7 @@ public class AccountController : Controller
                 await _signInManager.SignInAsync(user, isPersistent: false);
                 return RedirectToAction("Index", "Home");
             }
-            //AddErrors(result);
+           // AddErrors(result);
         }
 
         // If we got this far, something failed, redisplay form
@@ -60,21 +61,13 @@ public class AccountController : Controller
         {
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
             if (result.Succeeded)
             {
-                _logger.LogInformation(1, "User logged in.");
-                return RedirectToLocal(returnUrl);
-            }
-            if (result.RequiresTwoFactor)
-            {
-                return RedirectToAction(nameof(SendCode), new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
-            }
-            if (result.IsLockedOut)
-            {
-                _logger.LogWarning(2, "User account locked out.");
-                return View("Lockout");
-            }
+            //    _logger.LogInformation(1, "User logged in.");
+                return Redirect(returnUrl);
+            }    
+            
             else
             {
                 ModelState.AddModelError(string.Empty, "Invalid login attempt.");
@@ -86,10 +79,16 @@ public class AccountController : Controller
         return View(model);
     }
 
+    private IActionResult RedirectToLocal(string returnUrl)
+    {
+        throw new NotImplementedException();
+    }
+
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public ActionResult LogOff()
+    public async Task<IActionResult> LogOff()
     {
-        AuthenticationManager.SignOut();
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index","Home");
     }
 }
