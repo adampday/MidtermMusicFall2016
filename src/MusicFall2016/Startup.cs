efe6b5d90935813ad67a057f9dae91dc0a8da35d
daya1 <daya1@D017LAB-19>
2016-11-12 14:27:43 -0500

    first commit to new branch

--- src/MusicFall2016/Startup.cs
+++ src/MusicFall2016/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using MusicFall2016.Models;
-
+// new change
 namespace MusicFall2016
 {
     public class Startup
